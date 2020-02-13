b619e6d575705c26643161afabcbb614c7c7181e
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2020-02-13 16:12:29 -0500

    hello ----! my age is---

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let name = 'vero';
+	let age = 26;
+
+	function incrementAge() {
+		age += 1;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, my age is {age}</h1>
+<button on:click={incrementAge}>Change Age</button>
