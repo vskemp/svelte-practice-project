ff3921975bc96831e4042f54634dc230bb1a9b25
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2020-02-17 11:25:26 -0500

    added input boxex and image

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,30 @@
 <script>
-	let name = 'vero';
+	import ContactCard from "./ContactCard.svelte";
+
+	let name = 'Roni';
+	let title = "";
+	let image = "";
+	let description = "";
 	let age = 26;
+
+// $: structure complier that makes sure when we change name it runs the code again
+	$: uppercaseName = name.toUpperCase();
+	$: console.log(name);
+	$: if (name === 'Veronica') {
+		age = 31;
+	}
 
 	function incrementAge() {
 		age += 1;
+	}
+
+	function changeName() {
+		name = 'Veronica';
+	}
+
+	function nameInput(event) {
+		const enteredValue = event.target.value;
+		name = enteredValue;
 	}
 </script>
 
@@ -13,5 +34,19 @@
 	}
 </style>
 
-<h1>Hello {name}, my age is {age}</h1>
+<h1>Hello {uppercaseName}, my age is {age}</h1>
 <button on:click={incrementAge}>Change Age</button>
+<!-- <button on:click={changeName}>Change Name</button> -->
+<!-- <input type="text" value={name} on:input={nameInput}> -->
+<!-- shortcut: -->
+<input type="text" bind:value={name}  />
+<input type="text" bind:value={title} />
+<input type="text" bind:value={image} />
+<textarea rows="3" bind:value={description} />
+
+<ContactCard 
+userName={name}
+jobTitle={title} 
+description={description}
+userImage={image}
+/>
